40ced8cab593676583f66c9da215c4438e789759
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-08 19:46:15 -0500

    Refactoring and planning to implement basic server with tokio rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,65 @@
-#[macro_use]
-extern crate rustful;
+extern crate mio;
 
-//use std::error::Error;
+//use mio::deprecated::*;
+//use mio::{Token, EventSet};
+//use mio::{IoDesc, IoHandle, ReadHint, Interest, PollOpt};
+use mio::*;
+use mio::tcp::{TcpListener, TcpStream};
 
-//use rustful::{ Server, Context, Response, TreeRouter };
+const SERVER : Token = Token(0);
+const CLIENT : Token = Token(1);
 
-//mod example;
-mod tcp_example;
+struct MyHandler(TcpListener);
+
+impl Handler for MyHandler {
+    type Timeout = ();
+    type Message = ();
+
+    fn ready(&mut self,
+             event_loop: &mut EventLoop<MyHandler>,
+             token: Token,
+             _: EventSet)
+    {
+        match token {
+            SERVER => {
+                let MyHandler(ref mut server) = *self;
+                // Accept and drop?
+                let _ = server.accept();
+            }
+            CLIENT => {
+                event_loop.shutdown();
+            }
+            _ => panic!("Something bad happened.")
+        }
+    }
+}
 
 fn main() {
-    tcp_example::run_echo();
+
+    let addr = "127.0.0.1:8888".parse().unwrap();
+
+    // Setup the server socket
+    let server = TcpListener::bind(&addr).unwrap();
+
+    // Create an event loop
+    let mut event_loop = EventLoop::new().unwrap();
+
+    // Start listening for incoming connections
+    event_loop.register(
+        &server /* TcpListener */,
+        SERVER  /* Token */,
+        EventSet::readable(),
+        PollOpt::edge()
+    ).unwrap();
+
+    // Setup the client socket
+    let sock = TcpStream::connect(&addr).unwrap();
+
+    // Register the socket
+    event_loop.register(&sock, CLIENT, EventSet::readable(),
+                        PollOpt::edge()).unwrap();
+
+
+    event_loop.run(&mut MyHandler(server)).unwrap();
+
 }
--- src/server.rs
+++ src/server.rs
@@ -7,58 +7,16 @@ use mio::tcp::*;
 use std::io::Read;
 use std::io::Write;
 
+const SERVER : Token = Token(0);
+const CLIENT : Token = Token(1);
+
+// Todo: How does our program work?
+// 
+//
+
 /// Central state of the program, managing connections etc.
-pub struct TinServer {
-    //pub  
+pub struct Server {
+
 }
 
-pub fn run_echo() {
-    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
-    for buf_stream in listener.incoming().map(|stream| BufferedStream::new(stream)) {
-        match buf_stream {
-            Err(e) => { println!("failed: {}", e) }
-            Ok(stream) => {
-                println!("Startin' a connect!");
-                thread::spawn(move || {
-                    handle_client(stream);
-                });
-            }
-        }
-    }
-}
-
-fn handle_client(mut stream: BufStream<TcpStream>) {
-    let mut buf;
-    stream.write("Hello client, please insert JSON into me.");
-
-    loop {
-        // clear out the buffer so we don't send garbage
-        buf = [0; 4096];
-        let _ = match stream.read(&mut buf) {
-            Err(e) => panic!("Got an error: {}", e),
-            Ok(m) => {
-                if m == 0 {
-                    // we've got an EOF
-                    println!("Finished reading from client.");
-                    break;
-                }
-                println!("Read a buffer.");
-                m
-            },
-        };
-
-        // take JSON.
-        //if ()
-
-        // Response of some sort
-
-        match stream.write("Oh hai\n".to_string().as_bytes()) {
-            Err(_) => break,
-            Ok(_) => {
-
-                continue
-            }, // this is where a history would go.
-        }
-    }
-}
 
