0dbb8b578b5ec5e54741c4afdf295450f6c28b57
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-10 20:40:55 -0500

    Commit

--- src/server.rs
+++ src/server.rs
@@ -1,20 +1,16 @@
-extern crate tokio;
 // Example TCP
-
-// EventLoop, EventSet, Io,
-use mio::*;
-use mio::tcp::*;
-
-use std::io::Read;
-use std::io::Write;
 
 // Todo: How does our program work?
 // 
 //
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
 
 /// Central state of the program, managing connections etc.
 pub struct Server {
-
+    socket: TcpListener,
+    connections: HashMap<Token, Client>,
+    users: HashMap<Token, Client>, // for now we'll just have a hashmap of online users.
 }
 
 
