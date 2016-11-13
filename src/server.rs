4907db1ac89bf0ddbba66ed6417a7f3f54d606e9
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-13 00:30:08 -0500

    Providing an implementation for managing client connections

--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,51 @@
 // Example TCP
 
-// Todo: How does our program work?
-// 
-//
+use std::net::SocketAddr;
+use std::io::{Write, Read};
+use std::str::{from_utf8};
+
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::Core;
+
+type Username = String;
+
+struct User {
+    live_address: Vec<SocketAddr>,
+
+}
+
+struct TctServer {
+    socket: TcpListener,
+    online: HashMap<Username, User>
+}
+
+/// Current implementation of our message interpreter, ideally will be the
+/// XMPP stream.
+/// Remember, there is one connection per client. Regular XMPP chat's would
+/// allow for multiple simultaneous connections to the same stream, not sure
+/// if we want to do that.
+struct ClientStream {
+    stream: TcpStream,
+    addr: SocketAddr,
+}
+
+impl Write for Writer {
+    fn write(&mut self, buf : &[u8]) -> Result<usize, std::io::Error> {
+        print!("{} : {}", self.addr.port(), from_utf8(buf).unwrap().to_string());
+        //let msg = from_utf8(buf).unwrap().to_string();
+        //let val : Data = serde_json::from_slice(&buf);
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> Result<(), std::io::Error> {
+        Ok(())
+    }
+}
 
 /// Central state of the program, managing connections etc.
 pub struct Server {
     socket: TcpListener,
-    connections: HashMap<Token, Client>,
-    users: HashMap<Token, Client>, // for now we'll just have a hashmap of online users.
+    //connections: HashMap<Token, Client>,
+    //users: HashMap<Token, Client>, // for now we'll just have a hashmap of online users.
 }
 
 
