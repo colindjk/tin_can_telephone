4e42b020cd11646b12db8533372199f6be832943
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-13 00:28:43 -0500

    Next: Custom parsing for attributes!

--- src/xmpp.rs
+++ src/xmpp.rs
@@ -1,49 +1,54 @@
-//#![feature(custom_derive, plugin, test)]
-//#![feature(custom_attribute)]
-//#![plugin(serde_macros)]
-
-//extern crate serde;
-//extern crate serde_xml;
+// TODO: Implement reading XmlStream struct: An absraction over a TCP stream
+//       sending data back and forth, handling all setup beyond binding the
+//       tokio_core::net::TcpStream struct.
 
 use std::collections::HashMap; 
+
+use std::io::{Read, Write};
 
 use futures::{Future, Poll};
 use futures::stream::Stream;
 
-//use tokio_core::io::{copy, Io};
-use tokio_core::net::{TcpStream};
-//use tokio_core::reactor::Core;
-
-use data::Data;
-
-struct XmlParser {
-
-}
-
-// Helpful typedefs for the values read in and out of the list.
-type Ser<D> = Box<Fn(D)       -> Option<Vec<u8>>>;
-type De<D>  = Box<Fn(Vec<u8>) -> Option<D>>;
+use serde_xml::from_str;
+use serde_xml::value::{Element, from_value};
+use serde_xml::Error;
 
 /// This will contain a hashmap for the possible XML messages / formats.
 /// Type 'D' for the Data type which will be returned by the function
 /// passed in via HashMap.
-struct XmlStream<D> {
-    stream: TcpStream,
-    id: String,
-    host: String,
-    ser: HashMap<String, Ser<D>>, // probably will want to use slices here instead.
-    de: HashMap<String, De<D>>, 
+struct XmlStream<S> where S : Read + Write {
+    stream: S,
+    stanzas: Vec<String>, // Describes the various types of messages.
 }
 
-impl<D> XmlStream<D> {
+// Note:
+// XmlStreams will be unaware of domains, as domains are a characteristic of
+// network streams, an XmlStream should be looked at as a (asynch?) parser.
 
-}
-
-impl<D> Stream for XmlStream<D> {
-    type Item = Data;
-    type Error = Result<(), ()>;
-    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        Err(Err(())) // uhhh fix this l8er
+impl<S> XmlStream<S> where S : Read + Write {
+    /// Consumes a stream for the purpose of reading a 'stream' of XML elements
+    /// sent from a client.
+    fn new(stream : S) -> XmlStream<S> {
+        XmlStream {
+            stream: stream,
+            stanzas: vec![],
+        }
     }
 }
+
+//impl<S> Read
+
+impl<S> Stream for XmlStream<S> where S : Read + Write {
+    type Item = String;
+    type Error = String;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        Err("Nope".to_string()) // uhhh fix this l8er
+    }
+}
+
+// -- Old code:
+// Helpful typedefs for the values read in and out of the list.
+//type Ser<D> = Box<Fn(D)       -> Option<Vec<u8>>>;
+//type De<D>  = Box<Fn(Vec<u8>) -> Option<D>>;
 
