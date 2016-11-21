884efef1ca86dbb4b4aa7714db23c38347cf1f0e
kibbles <colindjk@gmail.com>
2016-11-21 13:39:56 -0500

    Implemented a working 'decode' and 'encode' function to read from input from the TCP stream

--- src/data.rs
+++ src/data.rs
@@ -1,33 +1,40 @@
 // This file will include the Data (struct or enum) which will represent the
 // different pieces of data which can be passed via HTTP style Request / Response
-pub type UserID = SocketAddr; // right now we'll just id by port number for ease
+pub type UserID = u16; // right now we'll just id by port number for ease
 
 use std::net::SocketAddr;
+use std::io::{Error as IoError, ErrorKind};
+//use std::result::
+
 use tokio_core::io::{Io, Codec, EasyBuf};
-use std::io::{Error};
 //use tokio_core::io::frame::*;
+
+use json::ser::{to_vec};
+use json::de::{from_slice};
 
 pub type Message = Result<Data, String>;
 
-#[deriving(sized)]
-#[derive(Clone)]
 /// The IMMUTABLE struct which is passed between threads etc in order
 /// to send and receive messages.
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Data {
-    Message(UserID, String),    // Regular old message to UserID
-    Request(UserID),            // friend request to user
-    GroupRequest(UserID),       // Group invite request
-    Error(String),              // Some sort of error?
+    Message { // Regular old message to UserID
+        id: UserID,
+        msg: String
+    },    
+    Request { id: UserID },         // friend request to user
+    GroupRequest { id: UserID },    // Group invite request
+    Error(String),                  // Some sort of error?
 }
 
 impl Data {
     /// Optionally returns the ID the message should be sent to.
     pub fn id(&self) -> Option<UserID> {
         match *self {
-            Data::Message(id, _)    => Some(id.clone()),
-            Data::Request(id)       => Some(id.clone()),
-            Data::GroupRequest(id)  => Some(id.clone()),
-            Data::Error(_)          => None,
+            Data::Message{ id, .. }     => Some(id.clone()),
+            Data::Request{ id }         => Some(id.clone()),
+            Data::GroupRequest{ id }    => Some(id.clone()),
+            Data::Error(_)              => None,
         }
     }
 }
@@ -40,39 +47,26 @@ impl Codec for DataParser {
     type Out = Data;
 
     /// Reads the EasyBuf.
-    fn decode(&mut self, buf: &mut EasyBuf) -> Result<Option<Self::In>, Error> {
-        panic!("unimplemented");
+    fn decode(&mut self, buf: &mut EasyBuf)
+        -> Result<Option<Self::In>, IoError>
+    {
+        if let Ok(val) = from_slice(buf.as_ref()) {
+            Ok(Some(val))
+        } else {
+            Err(IoError::new(ErrorKind::Other, "Decode error".to_string()))
+        }
     }
 
     /// Fills the buffer with the consumed 'Out' message.
     fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) {
-        panic!("unimplemented");
+        if let Ok(mut json) = to_vec(&msg) {
+            buf.append(&mut json);
+        }
     }
 
     /// Reads until the end of stream, 
-    fn decode_eof(&mut self, buf: &mut EasyBuf) -> Result<Self::In, Error> {
-        panic!("unimplemented");
+    fn decode_eof(&mut self, buf: &mut EasyBuf) -> Result<Self::In, IoError> {
+        panic!("Unimplemented")
     }
 }
-
-//#[deriving(Debug, Serialize, Deserialize)]
-//pub struct Message {
-    //user_id: UserID,
-    //message_body: String,
-    //flags: u64,
-//}
-
-//#[deriving(Debug, Serialize, Deserialize)]
-//pub struct InfoRequest {
-
-//}
-
-//#[deriving(Debug, Serialize, Deserialize)]
-//pub struct User {
-    //user_id: UserID, // will be an username / email address.
-//}
-
-//enum Info {
-
-//}
 
