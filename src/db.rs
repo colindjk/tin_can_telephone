5fb546d0e6d260f50a0ce81aec7478129343988d
kibbles <colindjk@gmail.com>
2016-11-27 13:22:03 -0500

    Added initialization method

--- src/db.rs
+++ src/db.rs
@@ -3,14 +3,36 @@
 
 use stanza;
 
-use mysql::prelude;
+use std::io;
 
-pub struct Database;
+use tokio_core::reactor::{Core};
+
+use my::prelude;
+use my::{
+    Pool,
+};
+
+pub struct Database {
+    core: Core,
+    pool: Pool,
+}
 
 impl Database {
-    pub fn init() -> Database {
-        Database
+
+    /// Init's a connection to the DB, returning a result of whether the
+    /// operation ran successfully or not.
+    pub fn init() -> Result<Database, io::Error> {
+        let core = Core::new()?;
+        let pool = Pool::new(
+            "mysql://root:Winter13!@localhost:3307", &core.handle());
+
+        let db = Database { core: core, pool: pool };
+
+        Ok(db)
     }
+
+    // thing
+    //pub fn 
 
 }
 
