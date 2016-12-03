887b285d86fc8a4afabc857338d62ffda7da21b8
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-12-03 18:30:45 -0500

    updated client python script

---
+++ client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import fileinput
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 3000
+BUFFER_SIZE = 1024
+
+user = "colin"
+
+login_creds = '{"LoginCredentials":{"user":"' + user + '"}\n}'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(login_creds)
+
+user_to = "someone"
+
+for line in fileinput.input():
+    line = line.strip()
+    line = line.decode('utf-8', 'ignore').encode('utf-8')
+
+    # Insert code to handle switching users that we're talking to
+
+    message =
+    '{"Message":{"to":"' + someone + '","from":"' + colin + '","msg":"' + line + '"}}\n'
+
+    s.send(message)
+
